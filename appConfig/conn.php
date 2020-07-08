53f78eadf174d62a741ba4e99211f0893946ba6f
Axel Saputra <axel@instance-1.us-central1-a.c.molten-gantry-269208.internal>
2020-07-08 01:14:53 +0000

    ignore the connection file

--- appConfig/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "spk";
-$koneksi = mysqli_connect($host, $username, $password, $database) or die("Koneksi gagal");
-?>
