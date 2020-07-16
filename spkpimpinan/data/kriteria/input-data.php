699bb7a19d6dc871ea30f9c021a080c11ec7c14f
axeltjs <axel.tjsaputra@vmlyr.com>
2020-07-16 12:09:59 +0800

    fix typo

--- spkpimpinan/data/kriteria/input-data.php
+++ spkpimpinan/data/kriteria/input-data.php
@@ -7,7 +7,7 @@ include"../../../appConfig/conn.php";
 
 		
 		//membaca kode barang terbesar
-	$sql = "SELECT max(id_KRITERIA) FROM KRITERIA";
+	$sql = "SELECT max(id_kriteria) FROM kriteria";
 	$query = mysqli_query($koneksi,$sql) or die (mysqli_error($koneksi));
  
 	$nip = mysqli_fetch_array($query);
