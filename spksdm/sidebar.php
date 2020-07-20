19f488709756a166aed3699b592327de1631db5e
axeltjs <axel.tjsaputra@vmlyr.com>
2020-07-20 14:15:30 +0800

    fix conn

--- spksdm/sidebar.php
+++ spksdm/sidebar.php
@@ -12,7 +12,7 @@
 
     <li> <a href="?load=k"><i class="icon icon-signal"></i> <span>DATA KRITERIA</span></a> </li>
     <li> <a href="?load=sub"><i class="icon icon-inbox"></i> <span>DATA SUB KRITERIA</span></a> </li>
-		<li><a href="?load=member"><i class="icon icon-pencil"></i> <span>DATA PESERTA TELLER</span></a></li>
+		<li><a href="?load=member"><i class="icon icon-pencil"></i> <span>DATA PESERTA ATLET</span></a></li>
 		<li><a href="?load=rekap"><i class="icon icon-th"></i> <span>DATA REKAP NILAI </span></a></li>
     <li><a href="?load=hitung"><i class="icon icon-th"></i> <span>PERHITUNGAN</span></a></li>
     <li><a href="?load=hasil"><i class="icon icon-fullscreen"></i> <span>HASIL PERHITUNGAN</span></a></li>
