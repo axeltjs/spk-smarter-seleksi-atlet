cdeed106f8e47158810fcb24377408962ba344e1
axeltjs <axel.tjsaputra@vmlyr.com>
2020-08-10 21:26:33 +0800

    fix minor

--- spksdm/data/member/edit-data.php
+++ spksdm/data/member/edit-data.php
@@ -123,70 +123,7 @@ echo"
               <label class='control-label'>Usia:</label>
               <div class='controls'>
                 <input type='text' class='span2'  name='txtusia' value='$_data[usia]' readonly />
-				";
-			  if (($_data['usia']>= 20 && $_data['usia'] <=25 )){
-						
-						echo"
-				Nilai <select name='txtn2' class='span2' required >
-				
-						
-                  <option  >$_data[n2]</option>
-				  
-				 
-					<option value='100' >100</option>
-					 
-					 <option value='99'>99</option>
-					 <option value='98'>98</option>
-					 <option value='97'>97</option>
-					 <option value='96'>96</option>
-					 <option value='95'>95</option>
-					 <option value='94'>94</option>
-					 <option value='93 >93</option>
-					 <option value='92' >92</option>
-					 <option value='91'>91</option>
-					 <option value='90' >90</option>
-					 <option value='89' >89</option>
-					 <option value='88'>88</option>
-					 <option value='87' >87</option>
-					 <option value='86' >86</option>
-					 <option value='85'>85</option>
-					 <option value='84' >84</option>
-					 <option value='83'>83</option>
-					 <option value='82'>82</option>
-					 <option value='81'>81</option>
-					 
-					 
-                </select>
-			  
-				";
-			  }elseif(($_data['usia']>= 26 && $_data['usia'] <=30 )){
-						
-						echo"
-				Nilai <select name='txtn2' class='span2' required >
-				
-						
-                  <option  >$_data[n2]</option>
-				  
-				 
-					<option value='80' >80</option>
-					 
-					 <option value='79'>79</option>
-					 <option value='78'>78</option>
-					 <option value='77'>77</option>
-					 <option value='76'>76</option>
-					 <option value='75'>75</option>
-					 <option value='74'>74</option>
-					 <option value='73 >73</option>
-					 <option value='72' >72</option>
-					 <option value='71'>71</option>
-					 <option value='70' >70</option>
-					 
-					 
-                </select>
-			  
-				";
-			  }
-				echo" 
+				 <input name='txtn2' type='hidden' value='".$_data[n2]."'>	
               </div>
             </div>
 <div class='control-group'>
